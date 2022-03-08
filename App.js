327f03dee157fada7f1838019b099bdc5fa23272
NURDIAH BASIHOL <diahhgl@gmail.com>
2022-03-09 03:01:36 +0800

    ok

--- App.js
+++ App.js
@@ -1,201 +1,57 @@
-import React from 'react';
-import {TextInput, StatusBar, StyleSheet, Text, View, Dimensions, Platform, ScrollView, AsyncStorage, Alert} from 'react-native';
-import { AppLoading } from 'expo';
-import ToDo from './ToDo';
-import uuidv1 from 'uuid/v1';
+import React, {useState} from 'react';
+import { Keyboard, ScrollView, StyleSheet, Text, View } from 'react-native';
+import TaskInputField from './components/TaskInputField';
+import TaskItem from './components/TaskItem.js';
 
-const { height, width } = Dimensions.get('window')
+export default function App() {
+  const [tasks, setTasks] = useState([]);
 
-export default class App extends React.Component {
-  state = {
-    newToDo: '',
-    loadedToDos: false,
-    toDos: {}
+  const addTask = (task) => {
+    if (task == null) return;
+    setTasks([...tasks, task]);
+    Keyboard.dismiss();
   }
-  componentDidMount = () => {
-    this._loadToDos();
-  }
-  render() {
-    const { newToDo, loadedToDos, toDos } = this.state;
-    if(!loadedToDos) {
-      return <AppLoading />
-    }
-    return (
-      <View style={styles.container}>
-        <StatusBar barStyle="light-content" backgroundColor={'transparent'} translucent={true} />
-        <Text style={styles.title}>Just To Do</Text>
-        <View style={styles.card}>
-          <TextInput 
-            style={styles.input} 
-            placeholder='New To Do' 
-            value={newToDo} 
-            onChangeText={this._controlNewToDo} 
-            placeholderTextColor={'#999'}
-            returnKeyType={"done"}
-            autoCorrect={false}
-            onSubmitEditing={this._addToDo}
-            underlineColorAndroid={"transparent"}
-          />
-          <ScrollView contentContainerStyle={styles.toDos}>
-            {Object.values(toDos).reverse().map(toDo => <ToDo key={toDo.id} {...toDo} deleteToDo={this._deleteToDo} uncompleteToDo={this._uncompleteToDo} completeToDo={this._completeToDo} updateToDo={this._updateToDo}/>)}
-          </ScrollView>
-        </View>
-      </View>
-    );
-  }
-  _controlNewToDo = text => {
-    this.setState({
-      newToDo: text,
 
-    })
-  };
-  _loadToDos = async () => {
-    try {
-      const toDos = await AsyncStorage.getItem("toDos");
-      const parsedToDos = JSON.parse(toDos);
-      console.log(toDos);
-      this.setState({
-        loadedToDos: true,
-        toDos: parsedToDos || {}
-      });
-    } catch (err) {
-      Alert.alert(err);
-      console.log(err);
-    }
-  };
-  _addToDo = () => {
-    const { newToDo } = this.state;
-    if (newToDo != '') {
-      this.setState(prevState => {
-        const ID = uuidv1();
-        const newToDoObject = {
-          [ID]: {
-            id: ID,
-            isCompleted: false,
-            text: newToDo,
-            createdAt: Date.now()
-          }
-        };
-        const newState = {
-          ...prevState,
-          newToDo: '',
-          toDos: {
-            ...prevState.toDos,
-            ...newToDoObject
-          }
-        };
-        this._saveToDos(newState.toDos);
-        return {...newState}
-      });
-    }
-  };
-  _deleteToDo = (id) => {
-    this.setState(prevState => {
-        const toDos = prevState.toDos;
-        delete toDos[id];
-        const newState  = { 
-          ...prevState, 
-          ...toDos 
-        };
-        this._saveToDos(newState.toDos);
-        return { ...newState };
-    });
-  };
-  _uncompleteToDo = (id) => {
-    this.setState(prevState => {
-      const newState = {
-        ...prevState, 
-        toDos: {
-          ...prevState.toDos,
-          [id]: {
-            ...prevState.toDos[id],
-            isCompleted: false
-          }
-        }
-      };
-      this._saveToDos(newState.toDos);
-      return { ...newState };
-    });
-  };
-  _completeToDo = (id) => {
-    this.setState(prevState => {
-      const newState = {
-        ...prevState, 
-        toDos: {
-          ...prevState.toDos,
-          [id]: {
-            ...prevState.toDos[id],
-            isCompleted: true
-          }
-        }
-      };
-      this._saveToDos(newState.toDos);
-      return { ...newState };
-    });
-  };
-  _updateToDo = (id, text) => {
-    this.setState(prevState => {
-      const newState = {
-        ...prevState, 
-        toDos: {
-          ...prevState.toDos,
-          [id]: {
-            ...prevState.toDos[id],
-            text: text
-          }
-        }
-      };
-      this._saveToDos(newState.toDos);
-      return { ...newState };
-    });
-  };
-  _saveToDos = (newToDos) => {
-    const saveToDos = AsyncStorage.setItem("toDos", JSON.stringify(newToDos));
+  const deleteTask = (deleteIndex) => {
+    setTasks(tasks.filter((value, index) => index != deleteIndex));
   }
+
+  return (
+    <View style={styles.container}>
+        <Text style={styles.heading}>TODO LIST</Text>
+      <ScrollView style={styles.scrollView}>
+        {
+        tasks.map((task, index) => {
+          return (
+            <View key={index} style={styles.taskContainer}>
+              <TaskItem index={index + 1} task={task} deleteTask={() => deleteTask(index)}/>
+            </View>
+          );
+        })
+      }
+      </ScrollView>
+      <TaskInputField addTask={addTask}/>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F23657',
-    alignItems: 'center',
+    backgroundColor: '#1E1A3C',
   },
-  title: {
-    color: 'white',
-    fontSize: 30,
-    marginTop: 40,
-    fontWeight: '200',
-    marginBottom: 30,
+  heading: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: '600',
+    marginTop: 30,
+    marginBottom: 10,
+    marginLeft: 20,
   },
-  card: {
-    backgroundColor: 'white',
-    width: width - 25,
-    flex: 1,
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'rgb(50, 50, 50)',
-        shadowOpacity: 0.5,
-        shadowRadius: 10,
-        shadowOffset: {
-          height: -1,
-          width: 0,
-        }
-      },
-      android: {
-        elevation: 3,
-      },
-    }),
-
+  scrollView: {
+    marginBottom: 70,
   },
-  input: {
-    padding: 20,
-    borderBottomColor: '#bbb',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    fontSize: 25,
-  },
-  toDos: {
-    alignItems: 'center',
+  taskContainer: {
+    marginTop: 20,
   }
 });
